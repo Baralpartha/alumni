40528b577a795d0fe1d350ea8c1810e81005d466
MD Mehedi Hasan <mehedih2204@gmail.com>
2024-10-16 00:40:26 +0600

    update

---
+++ lib/profession_list.dart
@@ -0,0 +1,19 @@
+// profession.dart
+
+List<Map<String, String>> professions = [
+  {'PROF_CODE': '01', 'PROF_DESC': 'Doctor'},
+  {'PROF_CODE': '02', 'PROF_DESC': 'Engineer'},
+  {'PROF_CODE': '03', 'PROF_DESC': 'Banker'},
+  {'PROF_CODE': '04', 'PROF_DESC': 'Private Service'},
+  {'PROF_CODE': '05', 'PROF_DESC': 'Govt Cadre'},
+  {'PROF_CODE': '06', 'PROF_DESC': 'Police'},
+  {'PROF_CODE': '07', 'PROF_DESC': 'Army'},
+  {'PROF_CODE': '08', 'PROF_DESC': 'Navy'},
+  {'PROF_CODE': '09', 'PROF_DESC': 'Air Force'},
+  {'PROF_CODE': '10', 'PROF_DESC': 'Business'},
+  {'PROF_CODE': '11', 'PROF_DESC': 'University Teacher'},
+  {'PROF_CODE': '12', 'PROF_DESC': 'College Teacher'},
+  {'PROF_CODE': '13', 'PROF_DESC': 'School Teacher'},
+  {'PROF_CODE': '14', 'PROF_DESC': 'Govt Service'},
+  {'PROF_CODE': '15', 'PROF_DESC': 'Customs Officer'},
+];
